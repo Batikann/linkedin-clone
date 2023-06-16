d0cc1d816d705089d7a96a3913696dcd6727f12a
Batikann <emiruar123@gmail.com>
2023-06-16 15:20:26 +0300

    gitignore update

--- src/api/AuthAPI.tsx
+++ src/api/AuthAPI.tsx
@@ -1,1 +1,5 @@
-export const LoginAPI = () => {}
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+const auth = getAuth()
+export const LoginAPI = (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+}
